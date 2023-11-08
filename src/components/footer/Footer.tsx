3c1ab13e98a1a54a1853505a81c29ea4a36d7f0a
VictorMFL <vm859222@gmail.com>
2023-11-07 22:27:39 -0300

    chore: showing the footer when data exists

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,22 +3,26 @@ import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import { ChangeEvent, useState } from "react";
 
-import * as S from './styles'
+import * as S from "./styles";
+import { useHeroesDataContext } from "@/context/data/useHeroesData";
 
 const Footer = () => {
   const [page, setPage] = useState(1); // Controla a página que o usuário está
+
+  const { data } = useHeroesDataContext();
 
   const { handleLoadMore } = usePaginationContext();
 
   const handleChange = (event: ChangeEvent<unknown>, value: number) => {
     setPage(value);
-    handleLoadMore(value)
+    handleLoadMore(value);
   };
 
   function scrollToTop() {
-    window.scrollTo({top: 0, behavior: 'smooth'});
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }
 
+  if (data.length == 0) return null;
   return (
     <S.Footer>
       <Stack spacing={2}>
